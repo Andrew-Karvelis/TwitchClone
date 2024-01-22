e9d8213fc44a79b0c51c268d455a35965ecff99d
Andrew-Karvelis <akarvelis.work@gmail.com>
2024-01-22 23:47:52 +1100

    Login, dashboard, stream, and chat functionality - Still to add features like unblock, search, homepage, userpage, about, and info.

---
+++ app/(browse)/[username]/_components/actions.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { toast } from "sonner";
+import { useTransition } from "react";
+
+import { onBlock, onUnBlock } from "@/actions/block";
+import { onFollow, unFollow } from "@/actions/follow";
+import { Button } from "@/components/ui/button";
+
+interface ActionsProps {
+    isFollowing: boolean;
+    userId: string;
+};
+
+export const Actions = ({
+    isFollowing,
+    userId,
+}: ActionsProps) => {
+  const [isPending, startTransition] = useTransition();
+
+  const handleFollow = () => {
+    startTransition(() => {
+      onFollow(userId)
+      .then((data) => toast.success(`You are now following ${data.following.username}`))
+      .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
+  const handleUnFollow = () => {
+    startTransition(() => {
+      unFollow(userId)
+      .then((data) => toast.success(`You have unfollowed ${data.following.username}`))
+      .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
+
+  const onClick = () => {
+    if (isFollowing) {
+        handleUnFollow();
+    } else {
+        handleFollow();
+    }
+  }
+
+    
+    const handleBlock = () => {
+      startTransition(() => {
+        onBlock(userId)
+        .then((data) => toast.success(`Blocked the user ${data.blocked.username}`))
+        .catch(() => toast.error("Something went wrong"));
+      });
+    };
+    
+  
+
+  return (
+    <>
+    <Button 
+    disabled={isPending} 
+    onClick={onClick} 
+    variant="primary">
+      {isFollowing ? "Unfollow" : "Follow"}
+    </Button>
+    <Button onClick={handleBlock} disabled={isPending}>
+      Block
+    </Button>
+    </>
+  );
+};
---
+++ lib/auth-service.ts
@@ -0,0 +1,41 @@
+import { currentUser } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+
+export const getSelf = async () => {
+  const self = await currentUser();
+
+  if (!self || !self.username) {
+    throw new Error("Unauthorised");
+  }
+
+  const user = await db.user.findUnique({
+    where: { externalUserId: self.id },
+  });
+
+  if (!user) {
+    throw new Error("Not found");
+  }
+  return user;
+};
+
+export const getSelfByUsername = async (username: string) => {
+  const self = await currentUser();
+
+  if (!self || !self.username) {
+    throw new Error("unauthorised");
+  }
+
+  const user = await db.user.findUnique({
+    where: { username },
+  });
+  if (!user) {
+    throw new Error("user not found");
+  }
+
+  if (self.username !== user.username) {
+    throw new Error("unauthorised")
+  }
+
+  return user;
+};
